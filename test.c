5dfedd4ae12b109ef9ebab4168a7f275d37c8869
Kyle Ehlers <kce0002@auburn.edu>
2018-09-19 13:25:33 -0500

    figuring out best way to store message in client12 using test.c

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	u_int32_t a = 300;
+	printf("\na10 = %d\n", a);
+	printf("a16 = %x\n", a);
+	char buf[32];
+	printf("sizeof a: %d\n", sizeof(a));
+	snprintf(buf, sizeof(a) + 1, "\n%02x\n", a);
+	buf[4] = '\0';
+	printf("STRING: \n%s\n", buf);
+	return 0;
+}
