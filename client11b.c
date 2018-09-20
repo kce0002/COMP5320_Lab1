9064738ba27dc5e04daabec637c15e26d757acd9
Kyle Ehlers <kce0002@auburn.edu>
2018-09-20 00:19:42 -0500

    collected all necessary data for the client, need to combine it all into packets to be sent.

--- client11b.c
+++ client11b.c
@@ -12,9 +12,6 @@
  * also keeps track of the round trip time of the message being sent
  * and received.
 */
-// TODO: Find out if program is supposed to use IP Address or hostname.
-// TODO: Figure out why first attempt fails.
-
 
 // Library includes:
 #include <stdlib.h>
@@ -27,19 +24,22 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <netdb.h>
+#include <stdint.h>
+#include <math.h>
 
 // Constants:
 #define SERV_PORT 10010
 #define MAXLINE 1024
+#define MAXPKTSZ 1038
+#define MAX 2
 
 int main(int argc, char **argv) {
 	
 	// Check for proper usage:
 	if (argc != 2) {
-		perror("Usage: UDPClient <IP address of the server>");
+		perror("Usage: ./client11b <server hostname>");
 		exit(1);
 	}
-	
 	// Variable declarations:
 	int socketfd;
 	int n;
@@ -49,46 +49,64 @@ int main(int argc, char **argv) {
 	struct timeval start, end;
 	struct hostent *he;
 	int rtt;
-	
-	// Prompt user for message:
-	printf("\nEnter message to be sent to server: ");
-	fgets(sendline, 1024, stdin);
-	
+	//char msg[MAXLINE + 1];
+	char msg[MAX + 2];
+
+	int16_t msgLength;
+	int32_t seqNum = 0;
+	long unsigned int timestamp;
+	const unsigned int MAXSEQNUM = (long unsigned int) pow(2, 32) - 1;
+	seqNum = (long unsigned int) pow(2, 32) - 3;
 	if ((he = gethostbyname(argv[1])) == NULL) {
-		perror("gethostbyname");
-		exit(1);
-	}
-	
-	// Create the UDP socket:
-	if ((socketfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("Problem in creating the socket");
+		perror("Error in resolving server IP Address.");
 		exit(2);
-	}
-	
-	// Prepare additional socket information:
-	memset(&servaddr, 0, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	//servaddr.sin_addr.s_addr = inet_addr(argv[1]);
-	servaddr.sin_addr = *((struct in_addr *)he->h_addr);
-	servaddr.sin_port = htons(SERV_PORT);
-	
-	gettimeofday(&start, NULL); // Start "timer"
-	sendto(socketfd, (char *) sendline, strlen(sendline), MSG_CONFIRM, (struct sockaddr *) &servaddr, sizeof(servaddr));
-
-	n = recvfrom(socketfd, (char *) recvline, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &servlen);
-		
-	gettimeofday(&end, NULL); // End "timer"
-
-	rtt = (end.tv_sec * 1000 + end.tv_usec / 1000) - (start.tv_sec * 1000 + start.tv_usec / 1000); // Calculate RTT	
-	
-	recvline[n] = '\0';
-	
-	// Output(stdout) echoed message and RTT:
-	printf("%s\n", recvline);
-	printf("Round Trip Time (RTT) = %d(ms)\n", rtt);
-	
-	// Close socket:
-	close(socketfd);
+	}	
+	do {
+		if ((socketfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+			perror("Problem in creating the socket.");
+			exit(3);
+		}
+		memset(&servaddr, 0, sizeof(servaddr));
+		servaddr.sin_family = AF_INET;
+		servaddr.sin_addr = *((struct in_addr *)he->h_addr);
+		servaddr.sin_port = htons(SERV_PORT);
+		printf("\nEnter message to be sent to server: ");
+		fgets(sendline, MAXLINE, stdin);
+		sendline[strlen(sendline) - 1] = '\0';
+		if (strlen(sendline) > MAXLINE) {
+			printf("Message too long. Try again.\n");
+		}
+		else {
+			gettimeofday(&start, NULL); // Start "timer"
+			//gettimeofday(&timestamp, NULL);
+			timestamp = start.tv_sec * 1000 + start.tv_usec / 1000;
+			printf("\ntimestamp: %ld\nbytes: %ld", timestamp, sizeof(timestamp));
+			if (seqNum < MAXSEQNUM) {
+				seqNum++;
+			}
+			else {
+				seqNum = 1;
+			}
+			printf("\nseqnum = %u\nsize = %ld\n", seqNum, sizeof(seqNum));
+			msgLength = 2 + sizeof(seqNum) + sizeof(timestamp) + strlen(sendline);
+			printf("\nmsgLength = %d\nsize = %ld\n", msgLength, sizeof(msgLength));
+			if (sendto(socketfd, (char *) sendline, strlen(sendline), MSG_CONFIRM, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+				perror("Problem in sending the packet.");
+				exit(4);
+			}
+			servlen = sizeof(servaddr); 
+			if ((n = recvfrom(socketfd, (char *) recvline, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &servlen)) < 0) {
+				perror("Problem in receiving the packet.");
+				exit(5);
+			}
+			gettimeofday(&end, NULL); // End "timer"
+			rtt = (end.tv_sec * 1000 + end.tv_usec / 1000) - (start.tv_sec * 1000 + start.tv_usec / 1000); // Calculate RTT	
+			recvline[n] = '\0';
+			printf("%s\n", recvline);
+			printf("Round Trip Time (RTT) = %d(ms)\n", rtt);
+			close(socketfd);
+		}
+	} while (1);
 
 	return 0;
 }
